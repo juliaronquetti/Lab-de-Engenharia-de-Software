862028a00211d8cae325116c35c2b8e59bc5294e
Julia <julia.ronquetti28@gmail.com>
2023-11-16 06:32:35 -0300

    'post'

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
-from .models import Profile
+from .models import Profile, Post
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -8,7 +8,24 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html') #chama o caminho para o html index
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile': user_profile}) #chama o caminho para o html index
+
+@login_required(login_url='signin')
+def upload(request):
+
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
 
 def signup(request):
     if request.method == 'POST':
@@ -66,4 +83,23 @@ def logout(request):
 
 @login_required(login_url='signin')
 def settings(request):
-    return render(request, 'setting.html')
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        if request.FILES.get('image') ==None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.save()
+
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile})
