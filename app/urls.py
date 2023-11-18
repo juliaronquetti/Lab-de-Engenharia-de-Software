acc351dfdf445e0d5043caa449faf0589ae0645c
Julia <julia.ronquetti28@gmail.com>
2023-11-18 17:01:16 -0300

    post feed e likes

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),#Define o caminho para a view que possui a função index
     path('settings', views.settings, name='settings'),
     path('upload', views.upload, name='upload'),
+    path('like-post', views.like_post, name='like-post'),
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout')
